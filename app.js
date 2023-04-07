53eedcc6336d80233ae82316fa5b32c8d3d16a4e
Fatyma Ferdous <125547915+FatymaFerdous@users.noreply.github.com>
2023-04-07 20:09:07 +0500

    TASKS DONE

---
+++ app.js
@@ -0,0 +1,89 @@
+             // task01
+// let num1= +prompt ("Enter your first number");
+// let num2= +prompt ("Enter your second number");
+// if (num1 > num2 ){
+// console.log ( num1 + " is larger" );
+// } else if (num1 < num2 ){
+//     console.log ( num2 + " is larger" );
+// } else {
+//     console.log ("both numbers are equal" );
+// }
+
+
+             // task02
+// let usernum= +prompt ("write a number");
+// if (usernum > 0 ) {
+//     console.log ("The sign is +");
+//     } else if (usernum < 0 ){
+//         console.log ("The sign is -");
+//     } else {
+//         console.log ("The sign is +");
+//     }
+
+
+             // task03                                                      
+// let num1= +prompt ("Enter your first number");
+// let num2= +prompt ("Enter your second number");
+// let num3= +prompt ("Enter your third number");
+// let num4= +prompt ("Enter your fourth number");
+// let num5= +prompt ("Enter your fifth number");
+
+// if (num1>num2 && num1>num3 && num1>num4 && num1>num5  ){
+//     console.log ( num1 + " is larger" );
+// }else if (num2>num1 && num2>num3 && num2>num4 && num2>num5  ){
+//     console.log ( num2 + " is larger" );
+// }else if (num3>num1 && num3>num2 && num3>num4 && num3>num5  ){
+//     console.log ( num3 + " is larger" );
+// }else if (num4>num1 && num4>num2 && num4>num3 && num4>num5  ){
+//     console.log ( num4 + " is larger" );
+// } else if (num5>num1 && num5>num2 && num5>num3 && num5>num4  ){
+//     console.log ( num5 + " is larger" );
+// }
+
+
+
+             // task04
+// for (let i=0; i<=15; i++){
+//     if (i % 2 ===0){
+//         document.write(i+" is even");
+//     }
+// else {
+//     document.write(i + " is odd" );
+// }
+// }
+
+
+             // task05
+// let usernum= +prompt ("write number to know your grade");
+// if (usernum <60 ) {
+//       console.log ("your grade is F");}
+// else if (usernum <70 ) {
+// console.log ("your grade is D");}
+// else if (usernum <80 ) {
+//     console.log ("your grade is C");}
+// else if (usernum <90 ) {
+// console.log ("your grade is B");}             
+// else if (usernum <=100 ) {
+//     console.log ("your grade is A");} 
+
+
+                // task06
+// for (let i=1; i<=100; i++){
+//     if (i %3 === 0){
+//         document.write("Fizz");
+//     }else if (i % 5 === 0){
+//         document.write("Buzz");
+// }else if (i % 3 === 0 && i % 5 === 0){
+//         document.write("FizzBuzz");
+// }else{
+//         document.write(i);  
+// }
+// }
+
+ 
+             // task07
+//  for (let i=1 ;i<=5; i++){
+//  for (let j = 1; j <= i; j++) {
+//     document.write("*")}
+//     document.write("<br>")
+//  }
